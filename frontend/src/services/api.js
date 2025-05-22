ac4c07b287534d773b0393009b1104a3309ac4d8
khushbu-8 <khushbuzalavadiya8@gmail.com>
2025-05-22 16:13:14 +0530

    save

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,10 @@
 import axios from 'axios';
 
 // const API = axios.create({ baseURL: 'http://localhost:4000' });
-const API = axios.create({ baseURL: 'https://employee-managment-system-nu-dun.vercel.app' });
+const API = axios.create({
+   baseURL: 'https://employee-managment-system-nu-dun.vercel.app',
+    withCredentials: true,
+});
 
 API.interceptors.request.use((req) => {
   const token = localStorage.getItem('token');
